d7231ba3208bc7afbec0236212205fa0d99b5dbc
ipeke94 <ipekerbakis@gmail.com>
2022-08-23 12:47:26 +0200

    Added style props to children component

--- mreact-ts/src/Children.tsx
+++ mreact-ts/src/Children.tsx
@@ -1,16 +1,16 @@
 /*
- * JSX.Element;
+ * JSX.Element; // Single object not supported arrays
  * JSX.Element | JSX.Element[];
- * React.ReactNode;
- * React.ReactChildren;
+ * React.ReactNode; -> support html elements, portals etc (React.ReactChild |  React..ReactChild[]  subset of ReactNode)
+ * React.ReactChildren; (it is not a type || built in to JS library ||  bunch of utility methods working with react children || it has bunch of helper methods )
  * React.ReactChild[];
  */
 
- type BoxProps = { children: React.ReactChild[] };
+ type BoxProps = { children: React.ReactNode, style?: React.CSSProperties };
 
- const Box = ({ children }: BoxProps) => {
+ const Box = ({ children , style = {}}: BoxProps ) => {
    return (
-     <section style={{ padding: "1em", border: "5px solid purple" }}>
+     <section style={{ padding: "1em", border: "5px solid green", ...style }}>
        {children}
      </section>
    );
@@ -21,7 +21,7 @@ function Children() {
      <Box>
        Just a string.
        <p>Some HTML that is not nested.</p>
-       <Box>
+       <Box style={{borderColor: 'red'}}>
          <h2>Another React component with one child.</h2>
        </Box>
        <Box>
