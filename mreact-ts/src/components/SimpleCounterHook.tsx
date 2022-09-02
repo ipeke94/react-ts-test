2ea29cb0479bfdd0e1158cb119738cc189b536ed
ipeke94 <ipekerbakis@gmail.com>
2022-09-03 00:26:54 +0200

    use functional call for changing value on simple counterHook component

--- mreact-ts/src/components/SimpleCounterHook.tsx
+++ mreact-ts/src/components/SimpleCounterHook.tsx
@@ -8,29 +8,29 @@ const SimpleCounterHook = ({ style = {} }: Style) => {
 
     const [count, setCount] = React.useState(0);
 
-    // const changeCount = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     setCount(+e.target.value);
-    // }
+    const changeCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCount(+e.target.value);
+    }
 
-    // const inc = (count: number) => count + 1;
-    // const dec = (count:number) => count -1;
-    // const reset = (count:number) => 0;
+    const inc = (count: number) => setCount(count + 1);
+    const dec = (count: number) => setCount(count - 1);
+    const reset = () => setCount(0);
 
     return (
         <main className="counter-container" style={{ ...style }}>
             <h1>Simple Counter with using React Hooks 🧮</h1>
             <section className="control-buttons wrapper">
                 <p className="counter-text" >{count}</p>
-                <button className="increment" onClick={() => setCount(count + 1)}>+</button>
-                <button className="decrement" onClick={() => setCount(count - 1)}>-</button>
-                <button className="reset" onClick={() => setCount(0)}>Reset</button>
+                <button className="increment" onClick={() => inc(count)}>+</button>
+                <button className="decrement" onClick={() => dec(count)}>-</button>
+                <button className="reset" onClick={() => reset()}>Reset</button>
             </section>
             <section className="control-buttons">
                 <form onSubmit={() => { }}>
                     <label htmlFor="set-to">
                         Set Count
                     </label>
-                    <input id="set-to" type="number" onChange={(e) => setCount(+e.target.value)} value={count} />
+                    <input id="set-to" type="number" onChange={(e) => changeCount(e)} value={count} />
                     <input type="submit" />
                 </form>
             </section>
